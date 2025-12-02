7237db337b244a0bce3f22b748ae176fb64c2283
knk1313 <yuta.kondo0207@gmail.com>
2025-12-02 16:00:47 +0900

    first setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+﻿module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es2020: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+﻿import "dotenv/config";
+
+function main() {
+  console.log("yahoo-news-x-bot started");
+
+  const xKey = process.env.X_API_KEY;
+  const openaiKey = process.env.OPENAI_API_KEY;
+
+  console.log("X_API_KEY exists:", !!xKey);
+  console.log("OPENAI_API_KEY exists:", !!openaiKey);
+}
+
+main();
